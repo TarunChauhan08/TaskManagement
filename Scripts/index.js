df858302d9faa2298c43bb262dd63ec65afe620d
TarunChauhan08 <tarunchauhan199908@gmail.com>
2022-08-17 15:12:57 +0530

    first commit

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+// We Use Const State Object so that we can change anything we want to
+const state={
+    taskList:[],
+};
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
